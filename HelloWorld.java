9514212afd3e9a47b084db2de12ae9dace96ed9d
claravox <claravox@users.noreply.github.com>
2021-04-12 15:20:17 -0700

    Add hello from Claire

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,9 @@
 public class HelloWorld{
-    public void main(String[] args) 
+    public static void main(String[] args) 
     {
-        printf("Kitty Zhuang!\n");
-        printf("I have been to Calpoly for almost 4 years\n");
-        printf("Hello, World!\n");
+        System.out.printf("Hello, World!\n");
+        System.out.printf("I am Kitty Zhuang!\n");
+        System.out.printf("I have been to Calpoly for almost 4 years\n");
+	System.out.printf("And this is Claire, saying hello as well!\n");
     }
 }
